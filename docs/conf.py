5fb6c1027e677707d7aae73d126ed64adfa8b592
Jos Verlinde <Jos.Verlinde@microsoft.com>
2024-07-14 20:27:23 +0200

    docs: Add AutoAPI Templates and config
    Signed-off-by: Jos Verlinde <Jos.Verlinde@microsoft.com>

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,36 @@ intersphinx_mapping = {
 from pathlib import Path
 from typing import Any, List
 
+#  https://sphinx-autoapi.readthedocs.io/en/latest/reference/config.html#confval-autoapi_options
+autoapi_options = [
+    "members",
+    "undoc-members",
+    # "inherited-members", # Display children of an object that have been inherited from a base class.
+    "private-members",  # _foo
+    "special-members",  # __foo
+    "show-inheritance",  # Display a list of base classes below the class signature.
+    "show-module-summary",  # include autosummary directives in generated module documentation
+    # "imported-members", $# For objects imported into a package, display objects imported from the same top level package or module.
+    # This option does not effect objects imported into a module.
+]
+autoapi_python_class_content = "class"  # Use only the class docstring for the class documentation.
+autoapi_member_order = "bysource"
+
+autoapi_generate_api_docs = True
+# Whether to generate API documentation. If this is False, documentation should be generated though the Directives.
+
+autoapi_keep_files = (
+    True  # Keep the AutoAPI generated files on the filesystem after the run. Useful for debugging.
+)
+
+if "exclude_patterns" not in globals():
+    exclude_patterns = ["autoapi_templates"]
+else:
+    exclude_patterns.append("autoapi_templates")
+
+# add all stubs to the autoapi_dirs
 stub_path = Path(__file__).parent / "stubs"
-autoapi_dirs = [Path("../my_package")]
+autoapi_dirs: List[Path] = []
 autoapi_dirs.extend(
     [p for p in stub_path.glob("*") if p.is_dir() and p.stem not in ["__pycache__"]]
 )
